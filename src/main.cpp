d945c246058748af28e6a868771ad8fffbb49a5c
Julian Heinken <hi@julianheinken.com>
2022-06-29 09:43:47 +0200

    overall singal chain improvment. more configs. ... long night, many tweaks. :)

--- src/main.cpp
+++ src/main.cpp
@@ -14,19 +14,47 @@
 //   -Microphone Gain
 
 // TODO-Optional:
-// -Check compresson like envelope folower
 
 #define BRIGHTNESS_OUT_PIN A0
 #define POTI_INPUT_PIN A1
 
-/*
 char ssid[] = "yourNetwork";  // your network SSID (name)
 char pass[] = "yourPassword"; // your network password (use for WPA, or use as key for WEP)
 int keyIndex = 0;             // your network key Index number (needed only for WEP)
 int led = LED_BUILTIN;
 int status = WL_IDLE_STATUS;
 WiFiServer server(80);
-*/
+
+enum PotiMode{
+  None,
+  LScale,
+  LBias,
+};
+
+static struct
+{
+  int mic_hardware_gain = 128;
+  double led_max_brightness = 1.0;
+  bool smoothed_volume_mode = false;
+  PotiMode mode = PotiMode::LScale;
+  float smoothModeAngularFreq = 16.0f;
+  float smoothModeAngularFreqAttackScale = 2.f;
+  float ambSmoothModeAngularFreq = 2.0f;
+  float loudnessBias = 1.25f;
+  float loudnessScale = 1.0f;
+  tDampedSpringMotionParams smoothModeParams;
+  tDampedSpringMotionParams smoothModeParamsAttack;
+  tDampedSpringMotionParams ambLoudnessParams;
+} config;
+
+static struct
+{
+  int ledFinalBrightness = 0;
+  float ledBrightness_vel = 0.0f, ledBrightness_pos = 0.0f;
+  float ambLoudness_vel = 0.0f, ambLoudness_pos = 0.0f;
+  unsigned long lastLoop = 0, deltaTime;
+  double envelopeFollowerGain = 1.0;
+} ctx;
 
 // default number of output channels
 static const char channels = 1;
@@ -39,11 +67,12 @@ short sampleBuffer[512];
 // Number of audio samples read
 volatile int samplesRead;
 
-float brightnessSmoothingAngularFreq = 64.0f;
-
-tDampedSpringMotionParams brightnessSmoothing;
-
 double mapd(double x, double in_min, double in_max, double out_min, double out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+float mapf(float x, float in_min, float in_max, float out_min, float out_max)
 {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
@@ -60,17 +89,31 @@ float InvLerp(float a, float b, float v)
 void BrightnesSmoothingCalculateParams()
 {
   CalcDampedSpringMotionParams(
-      &brightnessSmoothing,
-      0.005f,                         // delta time (0.005ms is kinda the duration for one loop)
-      brightnessSmoothingAngularFreq, // angular frequency
-      1.0f);                          // damping ratio
+      &config.smoothModeParams,
+      0.005f,                       // delta time (0.005ms is kinda the duration for one loop)
+      config.smoothModeAngularFreq, // angular frequency
+      1.0f);                        // damping ratio
+
+  CalcDampedSpringMotionParams(
+      &config.smoothModeParamsAttack,
+      0.005f,                       // delta time (0.005ms is kinda the duration for one loop)
+      config.smoothModeAngularFreq * config.smoothModeAngularFreqAttackScale, // angular frequency
+      1.0f);                        // damping ratio
+}
+
+void AmbLoudnessSmoothingCalculateParams()
+{
+
+  CalcDampedSpringMotionParams(
+      &config.ambLoudnessParams,
+      0.005f,                          // delta time (0.005ms is kinda the duration for one loop)
+      config.ambSmoothModeAngularFreq, // angular frequency
+      1.0f);                           // damping ratio
 }
 
 // ================================================================
 // Running HTTP Server
 // ================================================================
-
-/*
 void RunWebServer()
 {
   // compare the previous status to the current status
@@ -129,13 +172,57 @@ void RunWebServer()
 
             // the content of the HTTP response follows the header:
             client.print("AngFreq: <button class='red' type='submit' onmousedown='location.href=\"/AF_PLUS\"'>+</button>");
-            client.print(brightnessSmoothingAngularFreq);
+            client.print(config.smoothModeAngularFreq);
             client.print(" <button class='green' type='submit' onmousedown='location.href=\"/AF_MINUS\"'>-</button>");
             client.print("<br>");
 
-            int randomReading = analogRead(A1);
-            client.print("Random reading from analog pin: (PENIS) ");
-            client.print(randomReading);
+            client.print("AngFreq AttackScale: <button class='red' type='submit' onmousedown='location.href=\"/AF_AS_PLUS\"'>+</button>");
+            client.print(config.smoothModeAngularFreqAttackScale);
+            client.print(" <button class='green' type='submit' onmousedown='location.href=\"/AF_AS_MINUS\"'>-</button>");
+            client.print("<br>");
+
+            client.print("Amb AngFreq: <button class='red' type='submit' onmousedown='location.href=\"/AMB_AF_PLUS\"'>+</button>");
+            client.print(config.ambSmoothModeAngularFreq);
+            client.print(" <button class='green' type='submit' onmousedown='location.href=\"/AMB_AF_MINUS\"'>-</button>");
+            client.print("<br>");
+
+            client.print("Mic Gain: <button class='red' type='submit' onmousedown='location.href=\"/MIC_GAIN_PLUS\"'>+</button>");
+            client.print(config.mic_hardware_gain);
+            client.print(" <button class='green' type='submit' onmousedown='location.href=\"/MIC_GAIN_MINUS\"'>-</button>");
+            client.print("<br>");
+
+            client.print("Loudness Scale: <button class='red' type='submit' onmousedown='location.href=\"/L_SCALE_PLUS\"'>+</button>");
+            client.print(config.loudnessScale);
+            client.print(" <button class='green' type='submit' onmousedown='location.href=\"/L_SCALE_MINUS\"'>-</button>");
+            client.print("<br>");
+
+            client.print("Loudness Bias: <button class='red' type='submit' onmousedown='location.href=\"/L_BIAS_PLUS\"'>+</button>");
+            client.print(config.loudnessBias);
+            client.print(" <button class='green' type='submit' onmousedown='location.href=\"/L_BIAS_MINUS\"'>-</button>");
+            client.print("<br>");
+
+            client.print("Smooth Volume Mode: <button class='blue' type='submit' onmousedown='location.href=\"/SV_MODE\"'>+</button>");
+            client.print(config.smoothed_volume_mode);
+            client.print("<br>");
+
+
+            client.print("Potention Disabled: <button class='blue' type='submit' onmousedown='location.href=\"/P_TO_NONE\"'>+</button>");
+            client.print(config.mode == PotiMode::None);
+            client.print("<br>");
+
+            client.print("Potention To Bias: <button class='blue' type='submit' onmousedown='location.href=\"/P_TO_BIAS\"'>+</button>");
+            client.print(config.mode == PotiMode::LBias);
+            client.print("<br>");
+
+            client.print("Potention To Scale: <button class='blue' type='submit' onmousedown='location.href=\"/P_TO_SCALE\"'>+</button>");
+            client.print(config.mode == PotiMode::LScale);
+            client.print("<br>");
+
+            client.print("Max Led Brightness Time: <button class='red' type='submit' onmousedown='location.href=\"/LED_BRIGHT_P\"'>+</button>");
+            client.print(config.led_max_brightness);
+            client.print(" <button class='green' type='submit' onmousedown='location.href=\"/LED_BRIGHT_M\"'>-</button>");
+            client.print("<br>");
+
 
             // The HTTP response ends with another blank line:
             client.println();
@@ -152,16 +239,98 @@ void RunWebServer()
           currentLine += c; // add it to the end of the currentLine
         }
 
-        // Check to see if the client request was "GET /H" or "GET /L":
+        // smoothing attack scale
+        if (currentLine.endsWith("GET /AF_AS_PLUS"))
+        {
+          config.smoothModeAngularFreqAttackScale += 0.1f;
+          BrightnesSmoothingCalculateParams();
+        }
+        if (currentLine.endsWith("GET /AF_AS_MINUS"))
+        {
+          config.smoothModeAngularFreqAttackScale -= 0.1f;
+          BrightnesSmoothingCalculateParams();
+        }
+
+        // smoothing
         if (currentLine.endsWith("GET /AF_PLUS"))
         {
-          brightnessSmoothingAngularFreq += 1.0f;
+          config.smoothModeAngularFreq += 1.0f;
           BrightnesSmoothingCalculateParams();
         }
         if (currentLine.endsWith("GET /AF_MINUS"))
         {
-          brightnessSmoothingAngularFreq -= 1.0f;
+          config.smoothModeAngularFreq -= 1.0f;
           BrightnesSmoothingCalculateParams();
+        }
+
+        // AMB af smoothing
+        if (currentLine.endsWith("GET /AMB_AF_PLUS"))
+        {
+          config.ambSmoothModeAngularFreq += 0.25f;
+          AmbLoudnessSmoothingCalculateParams();
+        }
+        if (currentLine.endsWith("GET /AMB_AF_MINUS"))
+        {
+          config.ambSmoothModeAngularFreq -= 0.25f;
+          AmbLoudnessSmoothingCalculateParams();
+        }
+
+        // loudness scale
+        if (currentLine.endsWith("GET /L_SCALE_PLUS"))
+        {
+          config.loudnessScale += 0.01f;
+        }
+        if (currentLine.endsWith("GET /L_SCALE_MINUS"))
+        {
+          config.loudnessScale -= 0.01f;
+        }
+
+        // loudness bias
+        if (currentLine.endsWith("GET /L_BIAS_PLUS"))
+        {
+          config.loudnessBias += 0.2f;
+        }
+        if (currentLine.endsWith("GET /L_BIAS_MINUS"))
+        {
+          config.loudnessBias -= 0.1f;
+        }
+
+
+        // led
+        if (currentLine.endsWith("GET /LED_BRIGHT_P")) {
+          config.led_max_brightness += 0.1f;
+        }
+        if (currentLine.endsWith("GET /LED_BRIGHT_M")) {
+          config.led_max_brightness -= 0.1f;
+        }
+
+
+        // mic hardware gain
+        if (currentLine.endsWith("GET /MIC_GAIN_PLUS"))  {
+          config.mic_hardware_gain += 4;
+          PDM.setGain(config.mic_hardware_gain);
+        }
+        if (currentLine.endsWith("GET /MIC_GAIN_MINUS")) {
+          config.mic_hardware_gain -= 1;
+          PDM.setGain(config.mic_hardware_gain);
+        }
+
+// switch mode
+        if (currentLine.endsWith("GET /SV_MODE")){
+          config.smoothed_volume_mode = !config.smoothed_volume_mode;
+        }
+
+
+        if (currentLine.endsWith("GET /P_TO_NONE")) {
+          config.mode = PotiMode::None;
+        }
+
+        if (currentLine.endsWith("GET /P_TO_BIAS")) {
+          config.mode = PotiMode::LBias;
+        }
+
+        if (currentLine.endsWith("GET /P_TO_SCALE")) {
+          config.mode = PotiMode::LScale;
         }
       }
     }
@@ -170,82 +339,125 @@ void RunWebServer()
     Serial.println("client disconnected");
   }
 }
-*/
 
 // ================================================================
 // Processing Audio globals
 // ================================================================
 
-int ledFinalBrightness = 0;
-float ledBrightness_vel = 0.0f, ledBrightness_pos = 0.0f;
-unsigned long lastLoop = 0, deltaTime;
-
-double envelopeFollowerGain = 1.0;
 void ProcessAudio()
 {
   // Wait for samples to be read
   if (samplesRead)
   {
     unsigned long currentTime = millis();
-    deltaTime = currentTime - lastLoop;
-    lastLoop = currentTime;
+    ctx.deltaTime = currentTime - ctx.lastLoop;
+    ctx.lastLoop = currentTime;
 
-    double average = 0;
+    // compute the highest loudness over the past time
+    short min_value = 32767, max_value = -32768;
+    double average_value = 0.0;
     for (int i = 0; i < samplesRead; i++)
     {
-      average += (double)sampleBuffer[i];
+      short s = sampleBuffer[i];
+      min_value = min(min_value, s);
+      max_value = max(max_value, s);
+      average_value += (double)s;
     }
-    average /= (double)samplesRead;
+
+    int loudness_raw = (int)max_value - (int)min_value;
+    average_value /= (double)samplesRead;
     // Clear the read count
     samplesRead = 0;
 
-    float averageLoudness01 = InvLerp(0.0f, 32767.0f, (float)abs(average));
-     Serial.print("averageLoudness01:");
-     Serial.print(averageLoudness01);
-     Serial.println();
+    float loudness01 = InvLerp(0, 32767.0f, (float)loudness_raw);
+    float valueMin = InvLerp(-32767.0f, 32767.0f, (float)min_value);
+    float valueMax = InvLerp(-32767.0f, 32767.0f, (float)max_value);
 
-    UpdateDampedSpringMotion(&ledBrightness_pos, &ledBrightness_vel, averageLoudness01, brightnessSmoothing);
-
-   ledFinalBrightness = (int)(ledBrightness_pos * 255.0f);
+    // compute the "ambient" loudness
+    UpdateDampedSpringMotion(&ctx.ambLoudness_pos, &ctx.ambLoudness_vel, loudness01, config.ambLoudnessParams);
 
 
-    // Serial.print("ledBrightness out:");
-    // Serial.print(ledBrightness_pos);
+    int modifiedRaw = analogRead(POTI_INPUT_PIN);
+    float modifiedRawFloat = (float) modifiedRaw;
+    if(config.mode == PotiMode::LBias){
+     
+      float biasByPoti = mapf(modifiedRawFloat, 0.0f, 1023.0f, -2.0f, 2.0f);
+      config.loudnessBias = biasByPoti;
+      Serial.print("loudnessBias:");
+    Serial.print(biasByPoti);
+    Serial.println();
+    }
+
+    if(config.mode  == PotiMode::LScale){
+      float scaleByPoti = mapf((float) modifiedRaw, 0.0f, 1023.0f, 0.0f, 4.0f);
+      config.loudnessScale = scaleByPoti;
+    }
+
+
+    float normalizedLoudness = loudness01 * (0.5f / ctx.ambLoudness_pos);
+    float biasedNormalizedLoudness = max(0.0f, normalizedLoudness * (config.loudnessScale* 2.0f) - (0.5f + config.loudnessBias));
+
+    loudness01 = biasedNormalizedLoudness;
+
+  
+    
+    // gain = constrain(gain, 0, 255);
+    // PDM.setGain(gain);
+
+
+
+    // Serial.print("ambient loudness");
+    // Serial.print(ctx.ambLoudness_pos * 32767.0f);
     // Serial.println();
 
-    // Serial.print("deltaTime:");
-    // Serial.print(deltaTime);
+    // Serial.print("normalizedLoudness:");
+    // Serial.print(normalizedLoudness * 32767.0f);
+    // Serial.println();
+
+    // Serial.print("min value:");
+    // Serial.print(min_value);
+    // Serial.println();
+
+    // Serial.print("max value:");
+    // Serial.print(max_value);
     // Serial.println();
 
     // Serial.print("samplesRead:");
     // Serial.print(samplesRead);
     // Serial.println();
 
-    int modifiedRaw = analogRead(POTI_INPUT_PIN);
-    brightnessSmoothingAngularFreq = mapd(modifiedRaw, 0.0, 1023.0, 1.0, 96.0);
+    // config.smoothModeAngularFreq = mapd(modifiedRaw, 0.0, 1023.0, 1.0, 96.0);
+    // BrightnesSmoothingCalculateParams();
 
+    // double brightnessLimited = mapd(ledFinalBrightness, 0.0, 255.0, 0.0, 200.0);
 
-    BrightnesSmoothingCalculateParams();
-    //double brightnessLimited = mapd(ledFinalBrightness, 0.0, 255.0, 0.0, 200.0);
+    if (config.smoothed_volume_mode)
+    {
+      UpdateDampedSpringMotion(&ctx.ledBrightness_pos, &ctx.ledBrightness_vel, loudness01, config.smoothModeParams);
+      ctx.ledFinalBrightness = (int)(ctx.ledBrightness_pos * 255.0f);
+    }
+    else
+    {
+      const double thresshold = 0.5;
+      double overshoot = abs(thresshold - loudness01);
 
-    const double attack = 0.1;
-    const double release = 0.08;
-    const double thresshold = mapd(modifiedRaw, 0.0, 1023.0, 0.0, 0.1);
+      if (loudness01 > thresshold)
+      {
+        UpdateDampedSpringMotion(&ctx.ledBrightness_pos, &ctx.ledBrightness_vel, loudness01, config.smoothModeParamsAttack);
+      }
+      else
+      {
+        UpdateDampedSpringMotion(&ctx.ledBrightness_pos, &ctx.ledBrightness_vel, loudness01, config.smoothModeParams);
+                                                    // Serial.println("bellow");
+      }
 
-    if(averageLoudness01 > thresshold){
-      envelopeFollowerGain += attack;
-      Serial.println("above");
-    } else {
-      envelopeFollowerGain -= release;
-      Serial.println("bellow");
+      ctx.envelopeFollowerGain = max(0.0, min(1.0, ctx.envelopeFollowerGain));
+      ctx.ledFinalBrightness = (int)(ctx.ledBrightness_pos * 255.0f);
     }
 
-    envelopeFollowerGain = max(0.0, min(1.0, envelopeFollowerGain));
-    
-    ledFinalBrightness = (int)(envelopeFollowerGain * 255.0f);
-    Serial.print("envFollowGain:");
-    Serial.print(envelopeFollowerGain);
-    Serial.println();
+    // Serial.print("envFollowGain:");
+    // Serial.print(envelopeFollowerGain);
+    // Serial.println();
   }
 }
 /**
@@ -265,7 +477,6 @@ void onPDMdata()
   samplesRead = bytesAvailable / 2;
 }
 
-/*
 void printWiFiStatus()
 {
   // print the SSID of the network you're attached to:
@@ -322,9 +533,6 @@ void SetupWifi()
   // you're connected now, so print out the status
   printWiFiStatus();
 }
-
-*/
-
 // ================================================================
 // Setup
 // ================================================================
@@ -353,24 +561,20 @@ void setup()
     while (1)
       ;
   }
-
+  PDM.setGain(128);
   BrightnesSmoothingCalculateParams();
+  AmbLoudnessSmoothingCalculateParams();
+
+  SetupWifi();
 }
 
 void loop()
 {
 
   ProcessAudio();
-  // RunWebServer();
+  RunWebServer();
 
-  //int out = (int)(brightnessLimited * modifier);
-
-  // Serial.print("sound: ");
-  // Serial.print(brightnessLimited);
-  // Serial.print(", out: ");
-  // Serial.print(out);
-  // Serial.println();
-
-  ledFinalBrightness = min(ledFinalBrightness, 200);
+  double ledFinalBrightness = min((config.led_max_brightness * (double)ctx.ledFinalBrightness), 200);
+  //ledFinalBrightness = sqrt((ledFinalBrightness) / 255.0) * 255.0;
   analogWrite(BRIGHTNESS_OUT_PIN, ledFinalBrightness);
 }
